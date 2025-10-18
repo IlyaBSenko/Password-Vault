47e81cc9452689bc13c16eb72202325d6eea6a9e
Ilya Balatsenko <senkoet@gmail.com>
2025-10-17 20:25:46 -0400

    Working on password types, just have to go rq

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import random
 
-chars = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()?' # len = 37
+all_chars = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()?' # len = 37
+letters_only = 'abcdefghijklmnopqrstuvwxyz'         # 26
 
 password = ''
 for x in range(16):
-    password += random.choice(chars)
+    password += random.choice(all_chars)
 
-print(input("What kind of password would you like: "))
-print(password)
+print(len(letters_only))
+# user_inp = print(input("What kind of password would you like? Options: " \
+                        "Letters Only" \
+                        "Alphanumeric" \
+                        ""))
+
+
+
