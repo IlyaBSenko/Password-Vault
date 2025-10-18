32c65dd1d56a4c694f2a20f56b5ad03d58902db4
Ilya Balatsenko <senkoet@gmail.com>
2025-10-18 00:32:04 -0400

    Fixed code spacing, and added try block for length based option.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import random
 
+
 all_chars = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()?1234567890' 
 letters_only = 'abcdefghijklmnopqrstuvwxyz'         
 nums_only = '1234567890'
 special_only = '!@#$%^&*()?'                      
 chars_and_nums = 'abcdefghijklmnopqrstuvwxyz1234567890'
-all_chars_cases = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz!@#$%^&*()?1234567890'
 upper_lower = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+all_chars_cases = upper_lower + special_only + nums_only
+
 
 basic_types = ["Letters Only", "Alphanumeric"]                                        # low security
 intermediate_types = ["Alphanumeric + Special", "Mixed Case Letters", "Length Based"] # Moderate security
 advanced_types = ["Complex", "No Words/Patterns", "No Repeated Characters"]           # High Security
+
 
 all_types = [basic_types, intermediate_types, advanced_types]
 
@@ -18,10 +21,10 @@ prompt = "What kind of password would you like? Options: "
 
 option_list = [option for types in all_types for option in types] # each option in each type in all types list
 
+
 print(prompt)
 print(*option_list, sep="\n") # prints each option seperated by new line
 user_inp = input("What would you like?: ").strip()
-
 
 
 result = "Your Password Is: "
@@ -43,9 +46,16 @@ elif user_inp == "Mixed Case Letters":
     print(result + mixed_chars)
 
 elif user_inp == "Length Based":
-    lenPut = int(input("What length?: "))
-    len_res = ''.join(random.sample(all_chars_cases, lenPut))
-    print(result + len_res)
+    try:
+        lenPut = int(input("What length?: "))
+        max_unique = len(set(all_chars_cases))
+        if lenPut > max_unique:
+            print(f"Max unique length is {max_unique}. Try a smaller number.")
+        else:
+            len_res = ''.join(random.sample(all_chars_cases, lenPut))
+            print(result + len_res)
+    except ValueError:
+        print("Please enter a valid length. Thank you!")
 
 elif user_inp == "Complex":
     complex_res = ''.join(random.sample(all_chars_cases, 25))
@@ -59,4 +69,5 @@ elif user_inp == "No Repeated Characters":
     no_reps = ''.join(random.sample(all_chars_cases, 20))
     print(result + no_reps)
     
-
+else:
+    print("Sorry, I don't recognize that option. Please choose one of the given options and type it exactly as displayed. Thank you.")
