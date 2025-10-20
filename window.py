9b934460c1794b89b7ea939e84dc7e52f65f31aa
Ilya Balatsenko <senkoet@gmail.com>
2025-10-19 23:29:59 -0400

    Changed color of window, and progress bar

--- window.py
+++ window.py
@@ -2,6 +2,25 @@ import tkinter  as tk
 from tkinter import ttk
 import time
 
+# root of the tkinter tree
+root = tk.Tk()
+root.title("Password Generator")
+root.geometry("400x300")
+root.configure(bg='black')
+
+
+# progressbar widget
+progress = ttk.Progressbar(
+    root, 
+    style="DarkGreen.Horizontal.TProgressbar", 
+    orient="horizontal", 
+    length=300, 
+    mode="determinate",
+    maximum=100
+)
+progress.pack(pady=55)
+
+
 def start_progress():
     progress.start()
 
@@ -12,15 +31,24 @@ def start_progress():
         root.update_idletasks()
     progress.stop()
 
-root = tk.Tk()
-root.title("Place")
 
-# progressbar widget
-progress = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
-progress.pack(pady=20)
+# progress bar style ???
+style = ttk.Style()
+style.theme_use('clam')
+style.configure(
+    "DarkGreen.Horizontal.TProgressbar", 
+    background='#006400',             # fill color
+    troughcolor='black',               # background (empty area)
+    bordercolor="black",                # for the border to match the background
+    lightcolor = "#006400",           # removes 3D shading effect
+    darkcolor = "#006400"
+)
+
 
 # button for progress
-start_button = tk.Button(root, text="Start Progress", command=start_progress)
-start_button.pack(pady=10)
+start_button = tk.Button(root, text="Generate Password", command=start_progress)
+start_button.pack(pady=45)
+
+
 root.mainloop()
 
