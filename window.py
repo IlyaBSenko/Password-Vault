8f470731685225be3e1fc4ebed84247f25823a42
Ilya Balatsenko <senkoet@gmail.com>
2025-10-20 00:19:02 -0400

    Added view passwords button and changed placements for both main buttons:

--- window.py
+++ window.py
@@ -3,6 +3,7 @@
 import tkinter  as tk
 from tkinter import ttk
 import time
+# import main
 
 # root of the tkinter tree
 root = tk.Tk()
@@ -47,21 +48,32 @@ style.theme_use('clam')
 style.configure(
     "DarkGreen.Horizontal.TProgressbar", 
     background='#006400',             # fill color
-    troughcolor='black',               # background (empty area)
+    troughcolor='black',                # background (empty area)
     bordercolor="black",                # for the border to match the background
     lightcolor = "#006400",           # removes 3D shading effect
     darkcolor = "#006400"
 )
 
 
-# button for progress
+# view passwords button
+view_pw = tk.Button(
+    root, 
+    text="View Passwords", 
+    fg="darkgreen",
+    bg="black",
+    command=start_progress,
+)
+view_pw.pack(side="left", padx=55, pady=35)
+
+
+# generate password button
 start_button = tk.Button(root, 
     text="Generate Password", 
     fg="darkgreen",
     bg="black",
     command=start_progress,
 )
-start_button.pack(pady=35)
+start_button.pack(side="right", padx=55, pady=35)
 
 
 root.mainloop()
